7fe4336422889d7bb5f6a42bb71cd4fe8fd9fa3c
Noushad PP <to.noushadpp@gmail.com>
2018-08-15 20:04:20 +0530

    removed public path from webpack.prod.config for gh-pages rendering

--- src/modules/Calculator/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Calculator from "./index";
-
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render( <Calculator /> , div);
-});
