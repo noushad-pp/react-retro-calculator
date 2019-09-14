503f66d5461dc9fcd109bec6a34769de1fbc0452
Noushadpp <to.noushadpp@gmail.com>
2019-09-14 15:52:02 +0200

    Add GA-ID

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import ReactGA from 'react-ga';
 
 require('./favicon.ico');
 import Root from './root_container';
 import configureStore, { history } from './data/store/configureStore';
 const store = configureStore({});
+
+ReactGA.initialize('UA-123906240-1', { standardImplementation: true });
 
 render(
     <AppContainer>
